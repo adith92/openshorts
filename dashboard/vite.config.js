f1b1bf680fc46f628bd702084dffe2f0a70f1dd6
TritiumTech <adithyapratama2101@gmail.com>
2026-07-16 13:48:34 +0700

    chore: remove obsolete permissive Vite host setting

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://backend:8000',
